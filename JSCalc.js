31db2af37e3585eee745c10a271f99ba13418974
Moritisimor <devMoritisimor@proton.me>
2025-06-23 19:02:18 +0200

    Added stuff I did offline so far

---
+++ JSCalc.js
@@ -0,0 +1,34 @@
+const addButton = document.getElementById("AddButton");
+const subtractButton = document.getElementById("SubtractButton");
+const mulitplyButton = document.getElementById("MultiplyButton");
+const divideButton = document.getElementById("DivideButton");
+const ResultTxt = document.getElementById("ResultPar");
+
+function calculate(operator) {
+    let x = parseFloat(document.getElementById("xtxt").value);
+    let y = parseFloat(document.getElementById("ytxt").value);
+    let result;
+    if (operator === "add") { 
+        result = x + y;
+    } else if (operator === "subtract") {
+        result = x - y;
+    } else if (operator === "multiply") {
+        result = x * y;
+    } else if (operator === "divide") {
+        result = x / y;
+    }
+    if (isNaN(result)) {
+        alert("only enter numbers please!");
+        ResultTxt.textContent = "";
+    } else if (result === Infinity || result === -Infinity) {
+        alert("Do not divide by zero!")
+        ResultTxt.textContent = "";
+    } else {
+        ResultTxt.textContent = result.toString();
+    }
+}
+
+addButton.addEventListener("click", () => calculate("add"));
+subtractButton.addEventListener("click", () => calculate("subtract"));
+mulitplyButton.addEventListener("click", () => calculate("multiply"));
+divideButton.addEventListener("click", () => calculate("divide"));
