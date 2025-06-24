7db55468d3facb0ee14f30f68d94b4749490e946
Moritisimor <devMoritisimor@proton.me>
2025-06-24 19:14:09 +0200

    finished the guessing game. May improve, optimize or add more questions in the future.

--- JSScripts/GuessingGame.js
+++ JSScripts/GuessingGame.js
@@ -1,12 +1,42 @@
-const genQuestionButton = document.getElementById("genQuestionButton");
-const submitAnswerButton = document.getElementById("submitAnswerButton");
-const questionPar = document.getElementById("questionPar");
-const correctnessPar = document.getElementById("correctnessPar");
+const genQuestionButton     = document.getElementById("genQuestionButton");
+const submitAnswerButton    = document.getElementById("submitAnswerButton");
+const questionPar           = document.getElementById("questionPar");
+const correctnessPar        = document.getElementById("correctnessPar");
+let correctAnswer;
+
+function checkAnswer(correctAnswer) {
+    let answer = document.getElementById("answerTxt").value;
+    if (answer.toLowerCase() === correctAnswer) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 genQuestionButton.addEventListener("click", () => {
-    Math.floor(Math.random() * 11);
+    let randomQuestion = Math.floor(Math.random() * 5);
+    if (randomQuestion === 0) {
+        questionPar.textContent = "What does www in websites stand for?";
+        correctAnswer = "world wide web";
+    } else if (randomQuestion === 1) {
+        questionPar.textContent = "Who painted the Mona Lisa?";
+        correctAnswer = "leonardo davinci";
+    } else if (randomQuestion === 2) {
+        questionPar.textContent = "The bishop of Rome is known as?";
+        correctAnswer = "pope";
+    } else if (randomQuestion === 3) {
+        questionPar.textContent = "How many star wars movies are there?";
+        correctAnswer = "9";
+    } else if (randomQuestion === 4) {
+        questionPar.textContent = "What is the national sport of Japan?";
+        correctAnswer = "sumo wrestling"
+    }
 })
 
 submitAnswerButton.addEventListener("click", () => {
-
+    if (checkAnswer(correctAnswer)) {
+        alert("Your answer was correct!");
+    } else {
+        alert("Your answer was incorrect!");
+    }
 })
