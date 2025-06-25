6303c08b080ffd1427518f4c1f7f565445ba30a7
Moritisimor <devMoritisimor@proton.me>
2025-06-25 12:32:28 +0200

    Added buttons to paste the result into one of the input fields

--- JSScripts/JSCalc.js
+++ JSScripts/JSCalc.js
@@ -1,13 +1,15 @@
-const addButton         = document.getElementById("AddButton");
-const subtractButton    = document.getElementById("SubtractButton");
-const mulitplyButton    = document.getElementById("MultiplyButton");
-const divideButton      = document.getElementById("DivideButton");
-const ResultTxt         = document.getElementById("ResultPar");
+const addButton             = document.getElementById("addButton");
+const subtractButton        = document.getElementById("subtractButton");
+const mulitplyButton        = document.getElementById("multiplyButton");
+const divideButton          = document.getElementById("divideButton");
+const pasteIntoField1Button = document.getElementById("pasteResultInField1Button");
+const pasteIntoField2Button = document.getElementById("pasteResultInField2Button");
+const ResultTxt             = document.getElementById("resultPar");
+let result;
 
 function calculate(operator) {
-    let x = parseFloat(document.getElementById("xtxt").value);
-    let y = parseFloat(document.getElementById("ytxt").value);
-    let result;
+    let x = parseFloat(document.getElementById("xTxt").value);
+    let y = parseFloat(document.getElementById("yTxt").value);
     if (operator === "add") { 
         result = x + y;
     } else if (operator === "subtract") {
@@ -18,7 +20,7 @@ function calculate(operator) {
         result = x / y;
     }
     if (isNaN(result)) {
-        alert("only enter numbers please!");
+        alert("Only enter numbers please!");
         ResultTxt.textContent = "";
     } else if (result === Infinity || result === -Infinity) {
         alert("Do not divide by zero!")
@@ -27,6 +29,17 @@ function calculate(operator) {
         ResultTxt.textContent = result.toString();
     }
 }
+
+function pasteIntoField(htmlID) {
+    if (result === undefined || isNaN(result) || !isFinite(result)) {
+        alert("Calculate something first!")
+    } else {
+        document.getElementById(htmlID).value = result;
+    }
+}
+
+pasteIntoField1Button.addEventListener("click", () => pasteIntoField("xTxt"));
+pasteIntoField2Button.addEventListener("click", () => pasteIntoField("yTxt"));
 
 addButton.addEventListener("click", () => calculate("add"));
 subtractButton.addEventListener("click", () => calculate("subtract"));
