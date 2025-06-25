894579eabfdc8a6db40387eccff1f9e963fe3d6d
Moritisimor <devMoritisimor@proton.me>
2025-06-25 07:53:33 +0200

    Made several options valid

--- JSScripts/GuessingGame.js
+++ JSScripts/GuessingGame.js
@@ -11,24 +11,24 @@ genQuestionButton.addEventListener("click", () => {
         correctAnswer = "world wide web";
     } else if (randomQuestion === 1) {
         questionPar.textContent = "Who painted the Mona Lisa?";
-        correctAnswer = "leonardo davinci";
+        correctAnswer = ["leonardo da vinci" , "leonardo davinci"];
     } else if (randomQuestion === 2) {
         questionPar.textContent = "The bishop of Rome is known as?";
-        correctAnswer = "pope";
+        correctAnswer = ["pope", "the pope"];
     } else if (randomQuestion === 3) {
         questionPar.textContent = "How many star wars movies are there?";
-        correctAnswer = "9";
+        correctAnswer = ["9", "nine"];
     } else if (randomQuestion === 4) {
         questionPar.textContent = "What is the national sport of Japan?";
-        correctAnswer = "sumo wrestling"
+        correctAnswer = ["sumo", "sumo wrestling"];
     }
 })
 
 submitAnswerButton.addEventListener("click", () => {
-    let answer = document.getElementById("answerTxt").value;
+    let answer = document.getElementById("answerTxt").value.toLowerCase().trim();
     if (correctAnswer === undefined) {
         alert("Generate a question first!");
-    } else if (answer.toLowerCase() === correctAnswer) {
+    } else if (correctAnswer.includes(answer)) {
         alert("Your answer was correct!");
     } else {
         alert("Your answer was incorrect!");
